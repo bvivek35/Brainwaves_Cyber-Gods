65d5ff0b53f73e245ce5df80cee07054f1be91ae
Vivek Bharadwaj <bvivek35@gmail.com>
2014-11-09 04:14:03 +0530

    Added File IO and Server script

---
+++ app/src/main/java/com/brainwaves/cybergods/nbanking/FileIO.java
@@ -0,0 +1,34 @@
+package com.brainwaves.cybergods.nbanking;
+
+import java.io.*;
+import java.util.Scanner;
+/**
+ * Created by vb-deb on 9/11/14.
+ */
+class FileIO {
+
+
+    public static String readFrom() {
+
+        String s=null;
+        File file = new File("/sdcard/output");
+        try {
+            s = new Scanner(file).useDelimiter("\\Z").next();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return s;
+    }
+    public static void writeTo(String s)
+    {
+
+        try {
+            Writer w = new FileWriter("/sdcard/input");
+            w.write("hellohelloasdasdkj\n");
+            w.close()   ;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
